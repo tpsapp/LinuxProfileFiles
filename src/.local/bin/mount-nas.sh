2da0889569c435ef5f08661ec1d7f523c70b50f4
Thomas Sapp <tpsapp@hotmail.com>
2022-08-28 14:21:36 -0700

    added sript to unmount nas

--- src/.local/bin/mount-nas.sh
+++ src/.local/bin/mount-nas.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Mounting SAPPNAS...
 mkdir -p ~/SAPPNAS
-sudo mount //192.168.0.134/backup ~/SAPPNAS -o user=admin,pass=Ildaslm76,gid=tpsapp,uid=tpsapp
+sudo mount //192.168.0.134/backup ~/SAPPNAS -o user=admin,gid=tpsapp,uid=tpsapp
---
+++ src/.local/bin/umount-nas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo Unmounting nas...
+sudo umount -R ~/SAPPNAS
